4ecc5488296f6aa27520904fd3c8e1af795c6b1f
Julian Bayer <jdbayer09@hotmail.com>
2025-02-25 11:33:38 -0500

    configuracion del infinite scroll

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -47,7 +47,8 @@ class _HomeViewState extends ConsumerState<_HomeView> {
         MovieHorizontalListview(
           title: 'En Cines',
           subTitle: 'Lunes 20',
-          movies: nowPlayingMovies
+          movies: nowPlayingMovies,
+          loadNextPage: () => ref.read(nowPlayingMoviesProvider.notifier).loadNextPage(),
         )
       ],
     );

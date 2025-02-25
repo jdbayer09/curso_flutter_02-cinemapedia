706bb50538dae631d7e1317b490035ed8644f7eb
Julian Bayer <jdbayer09@hotmail.com>
2025-02-24 21:24:00 -0500

    Configuracion de la carga de la imagen

--- lib/presentation/widgets/movies/movies_slideshow.dart
+++ lib/presentation/widgets/movies/movies_slideshow.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:card_swiper/card_swiper.dart';
 import 'package:cinemapedia/domain/entities/movie_entity.dart';
 import 'package:flutter/material.dart';
@@ -36,6 +37,40 @@ class _Slide extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+
+    final borderRadius20 = BorderRadius.circular(20);
+
+    final decoration = BoxDecoration(
+      borderRadius: borderRadius20,
+      boxShadow: const [
+        BoxShadow(
+          color: Colors.black45,
+          blurRadius: 10,
+          offset: Offset(0, 10)
+          ),
+      ]
+    );
+
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 30),
+      child: DecoratedBox(
+        decoration: decoration,
+        child: ClipRRect(
+          borderRadius: borderRadius20,
+          child: Image.network(
+            movie.backdropPath,
+            fit: BoxFit.cover,
+            loadingBuilder: (context, child, loadingProgress) {
+              if (loadingProgress != null) {
+                return const DecoratedBox(
+                  decoration: BoxDecoration(color: Colors.black12)
+                );
+              }
+              return FadeIn(child: child);
+            },
+          )
+        ),
+      ),
+    );
   }
 }

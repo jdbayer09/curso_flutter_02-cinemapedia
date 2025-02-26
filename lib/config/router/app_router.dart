ffce99c6d63ba3052b5d2ebc61dc63ea55dce31b
Julian Bayer <jdbayer09@hotmail.com>
2025-02-26 16:48:39 -0500

    Configuracion de la ruta hija

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -7,7 +7,17 @@ final appRouter = GoRouter(
     GoRoute(
       path: '/',
       name: HomeScreen.name,
-      builder: (context, state) => HomeScreen()
-    )
+      builder: (context, state) => HomeScreen(),
+      routes: [
+        GoRoute(
+          path: 'movie/:id',
+          name: MovieScreen.name,
+          builder: (context, state) {
+            final movieId = state.pathParameters['id'] ?? 'no-id';
+            return MovieScreen(movieId: movieId);
+          }
+        )
+      ]
+    ),
   ]
 );
